437ef240dc55e088c75cde0c7ef4de3d77da7077
shetzero <dangzpz@gmail.com>
2016-09-17 00:45:18 +0200

    Add files via upload
    Arduino's code for getting temperature data with an "One Wire Digital Temperature Sensor - DS18B20"

---
+++ Temperature.ino
@@ -0,0 +1,22 @@
+#include <OneWire.h>  // Load Libraries
+#include <DallasTemperature.h>
+#define Pin 2  // Pin where we connect the sensor
+ 
+OneWire ourWire(Pin); 
+DallasTemperature sensors(&ourWire); 
+
+void setup() {  
+//Start Serial 
+delay(1000);
+Serial.begin(9600);
+sensors.begin();
+}
+
+void loop() {
+// Request temperature from sensor
+sensors.requestTemperatures();   
+// Print Temperature
+Serial.print(sensors.getTempCByIndex(0)); 
+Serial.println("º");
+delay(1000); // Repeat every second
+}
